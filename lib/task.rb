b3779ef5e73e57454f1349e792c3f9839a1c8293
a765904 <mharris@itriagehealth.com>
2015-07-08 16:20:23 -0600

    defined task class.

--- lib/task.rb
+++ lib/task.rb
@@ -1,4 +1,18 @@
 class Task
 
+	attr_accessor :title
+	attr_accessor :priority
+	attr_accessor :complete
+	alias_method  :complete?, :complete
+
+	def initialize(title, priority = 10) 
+		self.title = title 
+		self.priority = priority
+		self.complete = false 
+	end
+
+end
+
+if __FILE__ == $0
 
 end

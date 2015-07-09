8e5af0108b7c8f66daa78123a8e4c063d122ff57
a765904 <mharris@itriagehealth.com>
2015-07-09 15:06:27 -0600

    14 lines.

--- lib/task.rb
+++ lib/task.rb
@@ -1,22 +1,14 @@
 class Task
-
 	attr_accessor :title
 	attr_accessor :priority
 	attr_accessor :complete
 	alias_method  :complete?, :complete
-
 	def initialize(title, priority = 10) 
 		self.title = title 
 		self.priority = priority
 		self.complete = false 
 	end
-
 	def complete
 		self.complete = true	
 	end
-
-end
-
-if __FILE__ == $0
-
 end

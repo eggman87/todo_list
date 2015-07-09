51535c27c290eecf2115e26dbdba0528ce23213b
a765904 <mharris@itriagehealth.com>
2015-07-09 15:19:04 -0600

    minify.

--- lib/todo_list.rb
+++ lib/todo_list.rb
@@ -1,20 +1,11 @@
-#!/usr/bin/env ruby
-
 class TodoList
-
 	def initialize
 		@taskList = []
 	end 
-
 	def add(task) 
 		@taskList << task 
 	end
-
 	def tasks 
 		@taskList
 	end
-end
-
-if __FILE__ == $0
-
 end

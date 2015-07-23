22fb28e7d8e3d68ef6de831496aeb535a0bf3703
a765904 <mharris@itriagehealth.com>
2015-07-23 11:46:48 -0600

    todo list with persistence.

--- lib/task.rb
+++ lib/task.rb
@@ -1,14 +1,25 @@
+require "json"
+
 class Task
-	attr_accessor :title
-	attr_accessor :priority
-	attr_accessor :complete
+	attr_accessor :title, :priority, :complete
 	alias_method  :complete?, :complete
-	def initialize(title, priority = 10) 
+
+	def initialize(title, priority = 10, complete = false) 
 		self.title = title 
 		self.priority = priority
-		self.complete = false 
+		self.complete = complete 
 	end
+
 	def complete
 		self.complete = true	
 	end
-end
+
+	def to_json
+		hash = { 'title' => self.title, 'priority' => self.priority, 'complete' => self.complete }
+		hash.to_json
+	end
+
+	def self.fromJsonHash(jsonHash) 
+		Task.new(jsonHash['title'], jsonHash['priority'], jsonHash['complete'])
+	end
+end 
--- lib/todo_list.rb
+++ lib/todo_list.rb
@@ -1,5 +1,7 @@
+require "json"
+
 class TodoList
-	def initialize
+	def initialize( )
 		@taskList = []
 	end 
 	def add(task) 
@@ -7,5 +9,29 @@ class TodoList
 	end
 	def tasks 
 		@taskList
+	end
+
+	def save(file_name)
+		#serialize to string 
+		serialized_tasks = '['
+		serialized_tasks += self.tasks.map { |t| t.to_json }.join(',')
+		serialized_tasks += ']'
+
+		#write to file
+		File.open(file_name, "w") do |f|
+			f.write(serialized_tasks)
+		end
+	end
+	def self.load(file_name)
+		todoList = TodoList.new
+		taskHash = JSON.parse( IO.read(file_name))
+
+		#not proud of this, but it works! Imagine this can be done in much less lines. 
+		taskArray = taskHash.to_a
+		taskArray.each do |t|
+			hash = JSON.parse(t.to_json)
+			todoList.add(Task.fromJsonHash(hash))
+		end
+		todoList
 	end
 end
